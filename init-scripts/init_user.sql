1ca7683482bc14534c55533d0d4561fceb912c5f
emrekardaslar <emre.kardaslar@gmail.com>
2024-10-22 23:38:05 +0300

    add postgres for userService

---
+++ init-scripts/init_user.sql
@@ -0,0 +1,7 @@
+-- init_user.sql
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
